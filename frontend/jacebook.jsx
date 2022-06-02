558d5fc84f98771aa02e4f5a5577f0f5cf39b301
JaniceShih <janice.hc.shih@gmail.com>
2022-06-02 10:27:00 -0400

    Create new project

---
+++ frontend/jacebook.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+document.addEventListener("DOMContentLoaded", () => {
+  const root = document.getElementById("root");
+  ReactDOM.render(<h1>Welcome to JaceBook</h1>, root);
+});
