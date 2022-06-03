e77508c24ed562b6b2c04bb1f49452e61138548e
JaniceShih <janice.hc.shih@gmail.com>
2022-06-03 16:17:25 -0400

    navbar logo

--- frontend/components/navbar/navbar.jsx
+++ frontend/components/navbar/navbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
-
+import Avatar from '@mui/material/Avatar';
 
 class NavBar extends React.Component {
     constructor(props) {
@@ -9,20 +9,29 @@ class NavBar extends React.Component {
     
     render() {
         const {currentUser} = this.props;
+        let userImag =  <Avatar sx={{ height: '28px', width: '28px' }}/>
    
         return (
-            <div className='header'>
-                <div  className='header__left'>
+            <div className='navbar'>
+                <div  className='navbar__left'>
+                    <div>
+                         <Link to="/">
+                             <img src={window.jacebook} className="navbar__logo" />
+                        </Link>
+                    </div>
                    
                 </div>
 
-                <div  className='header__center'>
+                <div  className='navbar__center'>
                    
                 </div>
 
-                <div  className='header__right'>
-                    <div className="header__info">
-                        {currentUser.fname}
+                <div  className='navbar__right'>
+                    <div className="navbar__info">
+                        <div className="navbar__info--user"> 
+                                <div className="navbar__info--userimg">{userImag}</div>
+                                <div className="navbar__info--username">{currentUser.fname}</div>                                
+                            </div>
                      
                         <div onClick={this.props.logout}>
                                <p>
