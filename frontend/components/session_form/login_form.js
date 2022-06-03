160d17be7b5493d3a9195a367ebf1355cf879c6d
JaniceShih <janice.hc.shih@gmail.com>
2022-06-03 10:32:19 -0400

    login form

--- frontend/components/session_form/login_form.js
+++ frontend/components/session_form/login_form.js
@@ -17,6 +17,30 @@ class LoginForm extends React.Component {
                 </div>
 
                 <div className="login-box">
+                    <div>
+                        <input type="text" class="input__field login" name="email" id="email" data-testid="royal_email" placeholder="Email or phone number" autofocus="1" aria-label="Email or phone number" />
+
+                        <input type="text" class="input__field login" name="password" id="password" data-testid="royal_passwordl" placeholder="Password" autofocus="2" aria-label="Password" />
+                    </div>
+
+                    <div>                        
+                        <button className="btn btn--primary btn--login" onClick={this.handleSubmit}> 
+                                Log In 
+                        </button>
+                    </div>
+
+                    <div className="login__demo">                        
+                            <a onClick={this.loginDemo} className="link-demo"> Demo Log In </a>                                                
+                    </div>
+
+                    <div className="login__create">
+                        
+                        <button 
+                            className="btn btn--secondary btn--create"
+                            onClick={this.openSignupModal}>
+                            Create new account 
+                        </button>
+                    </div> 
 
                 </div>
 
