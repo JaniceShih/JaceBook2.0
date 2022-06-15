8c2f47a90a65cc7149ab76adf6634cbec59865d8
JaniceShih <janice.hc.shih@gmail.com>
2022-06-15 19:33:16 -0400

    remove logger

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -5,7 +5,7 @@ import rootReducer from "../reducers/root_reducer";
 
 
 const configureStore = (preloadedState={}) => {
-    return createStore(rootReducer, preloadedState, applyMiddleware(thunk, logger));
+    return createStore(rootReducer, preloadedState, applyMiddleware(thunk));
 };
 
 export default configureStore;
